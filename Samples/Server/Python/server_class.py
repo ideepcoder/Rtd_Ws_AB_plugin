19e0d1ab30cda0e4e45781ccc965d35036fa8c89
SpandexMan <admin@soundtherapeutics.net>
2025-03-08 13:36:01 +0800

    Interim commmit. For changing of broadcast_count method to accomodate new approach

--- Samples/Server/Python/server_class.py
+++ Samples/Server/Python/server_class.py
@@ -90,7 +90,7 @@ class RTDServer(PubSub):
         self.max_tickers        = 50           ## maximum concurrent ticks Try 1000 :) no problem
 
         self.keep_running     = True   ## True=Server remains running on client disconnect, False=Server stops on client disconnect
-        self.add_remove_list = []        ## simulate add and remove symbol command
+        self.websocket_subscription_list = []        ## simulate add and remove symbol command
 
         self.timezone = pytz.timezone('Australia/Perth')
 
@@ -111,6 +111,7 @@ class RTDServer(PubSub):
             "kraken":ccxtpro.kraken()
         }
 
+        self.websocket_data_list = []
 
     ## Proper way is for handler to have 2 task threads for send() and recv(),
     # in this case Handler thread work for sending.
@@ -162,29 +163,24 @@ class RTDServer(PubSub):
                                         print("Bananas")
                                         sym = json_message['arg'] if ' ' not in json_message['arg'] else (json_message['arg'].split(' '))[0]
                                         json_message['arg'] = f"y {sym} 2" if json_message['cmd']=='bfauto' else f"y {sym} 5"
-                                        #await self.broadcast( self.some_historical_data( json_message ) )
                                         await self.get_historic_data(json_message)
-
                                         json_message['sym'] = "addsym"
                                         json_message['arg'] = sym
-                                        self.add_symbol( json_message )
+                                        self.add_symbol(json_message)
 
                                     elif json_message['cmd'] == 'bfsym':
-                                        #Sock_send_Q.put( json_message )    ## real code should use Queue as buffer, separate thread/async
-                                        ##  json_message = {"cmd":"bfsym", "arg":"y SYM1 3 1"}
                                         print("Apples")
                                         await self.get_historic_data(json_message)
-                                        #await self.broadcast( self.some_historical_data( json_message ) )
 
                                     elif json_message['cmd'] == "addsym":
-                                        print("Pears") #This flow is when active sym is clicked inthe context menue of the plugin
+                                        print("Pears") #This flow is when active sym is clicked in the context menu of the plugin
                                         jr = self.add_symbol( json_message )
                                         await self.broadcast( jr )
 
                                     elif json_message['cmd'] == "remsym":
                                         print("oranges")
                                         jr = self.rem_symbol( json_message )
-                                        #await self.broadcast( self.some_historical_data( jr ) )
+                                        await self.broadcast( jr)
 
                                     else:
                                         print( f"unknown cmd in {message}")
@@ -212,102 +208,13 @@ class RTDServer(PubSub):
         return
 
 
-    def some_historical_data(self, json_message):
-        '''simulate some historical data'''
-        print(f"Some historical data for \n{json_message}")
-        ## 10:unix timestamp,       // 11:unix millisec timestamp,
-        ## 20:"20171215 091500",    // 21:"2017-12-15 09:15:00", // 22:"2017-12-15","09:15:00",
-        ## 30:20171215,91500,       // 31: 20171215,0,(EoD)
-        DtFormat = 30   ## {unix: 10, 11,} {str:20, 21, 22,} {int: 30(default), }
-
-        try:
-            t:str = json_message['arg']
-            t = t.split(' ')        ## reserved1 symbol_name date_from date_to timeframe ## to-do: make a json standard format
-
-            ## SAMPLE using DF, how to mainpulate type etc
-            hd_str =  '[["2024-06-11","10:00:00",22.1,22.5,22.1,22.8,12,0],["2024-06-12","10:05:00",22.8,22.7,22.3,22.4,28,0],\
-                        ["2024-06-13","10:10:00",22.8,22.7,22.3,22.4,28,0],["2024-06-14","10:15:00",22.8,22.7,22.3,22.4,28,0],\
-                        ["2024-06-15","10:20:00",22.8,22.7,22.3,22.4,28,0]]'
-
-            df = pd.DataFrame( json.loads( hd_str) )                    ## simulate DF
-            #df['oi'] = 0
-
-            if DtFormat in [30, 31, 32]:                                ## sample conversion
-                df.columns   = df.columns.astype(str)                       ## change type of columns from int to str
-
-                df['0'] = df['0'].str.replace('-','')
-                df['1'] = df['1'].str.replace(':','')
-                df      = df.astype( {"0": "int32", "1": "int32"} )
-
-
-            ## fail safe
-            if len(t) != 3: t = ["y", "SYM2", 1]
-
-            ## simulate error resopnse message in backfill
-            if t[1]=="SYM2":
-                ## SYM2 is also a bad json RTD (note)
-                jsWs = {"cmd":"bfsym","code":404,"arg":"example of backfill error in "+t[1]}
-                return json.dumps( jsWs, separators=(',', ':') )
-
-            ## simulate ASCII import response message
-            #elif t[1]=="SYM6":
-            #    jsWs = {"cmd":"bfsym","code":200,"arg":"example ASCII import"+t[1],"file":"D:\\test_ascii_import.txt","format":"D:\\test_ascii.format"}
-            #    return json.dumps( jsWs, separators=(',', ':') )
-
-            #### This is backfill data generator, DF above just for illustration   ####
-
-            jsWs = {'hist':t[1], 'format':"dtohlcvi"}           ## unix: "uohlcvi", you can re-arrange the fields
-
-            ## Sample using DF.         make sure columns match the format
-            #jsWs['bars'] = 0
-            ## join to form json string with df output
-            '''jsStr = json.dumps( jsWs, separators=(',', ':') )   ## , default=int argument
-            jsStr = jsStr[:-2]
-            jsStr = jsStr + df.to_json( orient="values" )
-            jsStr = jsStr + '}'        
-            return jsStr'''
-
-            ### Below is a dummy historical data generator  ###
-            jsWs['bars'] = []
-
-            bfdays      = int( t[2] )   # days argument
-            BarsPerDay  = 3             # no of bars to generate
-            tf          = 1             # timeframe in minutes
-            dt          = datetime.datetime.now()
-            dt          = dt - datetime.timedelta( days=bfdays, minutes=( (bfdays+1)*BarsPerDay ) )
-
-            i  = 0
-            while i <= bfdays:
-                j = 0
-                while j < BarsPerDay:
-
-                    jsWs['bars'].append([int( dt.strftime('%Y%m%d') ), int( dt.strftime('%H%M00') ),
-                                         20 + self.random_generator(), 40 - self.random_generator(), 10 + self.random_generator(), 18 + self.random_generator(), 100 + self.random_generator(100, 500), 0])
-
-                    ## Unix time example, change format string above [use u = localtime() or g=gmtime() c++]
-                    #jsWs['bars'].append( [ int( dt.timestamp()), 20+r(),40-r(),10+r(),18+r(),100+r(100,500),0] )
-
-                    dt = dt + datetime.timedelta( minutes=tf )
-                    j +=1
-
-                dt = dt + datetime.timedelta( days=1 )
-                i += 1
-            print("Line 287")
-            jsws_json = json.dumps( jsWs, separators=(',', ':') )    ## remove space else plugin will not match str
-            print(jsws_json, f"Data type: {type(jsws_json)}")
-            return jsws_json
-
-        except Exception as e:
-            return repr(e)
-
-
     ## simulate subscribe
     def add_symbol(self, json_message):
         try:
             json_copy = copy.deepcopy(json_message)
             sym = json_copy['arg']
-            if sym not in self.add_remove_list:
-                self.add_remove_list.append(sym)
+            if sym not in self.websocket_subscription_list:
+                self.websocket_subscription_list.append(sym)
                 json_copy['code'] = 200
                 json_copy['arg']  = sym + " subscribed ok"
             else:
@@ -323,11 +230,11 @@ class RTDServer(PubSub):
         json_copy = copy.deepcopy(json_message)
         sym = json_copy['arg']
 
-        if sym not in self.add_remove_list:
+        if sym not in self.websocket_subscription_list:
             json_copy['code'] = 400
             json_copy['arg']  = sym + " not subscribed"
         else:
-            self.add_remove_list.remove(sym)
+            self.websocket_subscription_list.remove(sym)
             json_copy['code'] = 200
             json_copy['arg']  = sym + " unsubcribed ok"
 
@@ -340,32 +247,41 @@ class RTDServer(PubSub):
 
 
     async def broadcast_messages_count(self):
-        s1=s2=s3=0
-        v1=v2=v3=0
-        pTm = 0
         try:
             while not self.stop_threads:
                 await asyncio.sleep(self.sleep_time)    #simulate ticks in seconds
-                dt   = datetime.datetime.now()
-
-                t    = dt.hour * 10000 + int(dt.minute / self.timeframe) * self.timeframe * 100
-                d    = int( dt.strftime('%Y%m%d') )
-
-                if( pTm != t):
-                    v1 =self.random_generator(3, 5); v2 =self.random_generator(2, 3); v3 =self.random_generator(1, 2); pTm = t;                    # bar vol reset
-                    if( self.inc_sym and self.ticker_count <= self.max_tickers):
-                        self.ticker_count += 1; print(self.ticker_count)
-
-                else:   v1+=self.random_generator(3, 5); v2+=self.random_generator(2, 3); v3+=self.random_generator(1, 2)                          # bar vol cum
-                s1+=v1; s2+=v2; s3+=v3                #total vol
-
+                t = 100000
+                d= 20250803
+                v1 = v2 = v3 = 0
+                s1 = s2 = s3 = 0
                 ## Open intentionally kept random, SYM2 test bad symbol
                 #data = []
                 ##'n', 'd', 't', 'o', 'h', 'l', 'c', 'v', 'oi', 's','pc','bs','bp','as','ap' (s=total vol, pc=prev day close bs,bp,as,ap=bid ask )
                 self.random_generator = self.random_generator
-                data = [{"n": "SYM1", "t":t, "d":d, "c": self.random_generator(1, 9), "o": self.random_generator(1, 9), "h": 9, "l": 1, "v": v1, "oi": 0, "bp": self.random_generator(1, 5), "ap": self.random_generator(5, 9), "s": s1, "bs":1, "as":1, "pc":1, "do":4, "dh":9, "dl":1}
-                    , {"n": "", "t":t, "d":d, "c": self.random_generator(10, 19), "o": self.random_generator(10, 19), "h": 19, "l": 10, "v": v2, "oi": 0, "bp": self.random_generator(10, 15), "ap": self.random_generator(15, 19), "s": s2, "pc":10, "do":15, "dh":19, "dl":10}
-                    , {"n": "SYM3", "t":t, "d":d, "c": self.random_generator(20, 29), "o": self.random_generator(20, 29), "h": 29, "l": 20, "v": v3, "oi": 0, "bp": self.random_generator(20, 25), "ap": self.random_generator(25, 29), "s": s3, "pc":22, "do":28, "dh":29, "dl":20}]
+                data = [
+                        {
+                        "n": "SYM1",
+                        "t":t,
+                         "d":d,
+                         "c": self.random_generator(1, 9),
+                         "o": self.random_generator(1, 9),
+                         "h": 9,
+                         "l": 1,
+                         "v": v1,
+                         "oi": 0,
+                         "bp": self.random_generator(1, 5),
+                         "ap": self.random_generator(5, 9),
+                         "s": s1,
+                         "bs":1,
+                         "as":1,
+                         "pc":1,
+                         "do":4,
+                         "dh":9,
+                         "dl":1
+                        },
+                    {"n": "", "t":t, "d":d, "c": self.random_generator(10, 19), "o": self.random_generator(10, 19), "h": 19, "l": 10, "v": v2, "oi": 0, "bp": self.random_generator(10, 15), "ap": self.random_generator(15, 19), "s": s2, "pc":10, "do":15, "dh":19, "dl":10},
+                    {"n": "SYM3", "t":t, "d":d, "c": self.random_generator(20, 29), "o": self.random_generator(20, 29), "h": 29, "l": 20, "v": v3, "oi": 0, "bp": self.random_generator(20, 25), "ap": self.random_generator(25, 29), "s": s3, "pc":22, "do":28, "dh":29, "dl":20}
+                ]
 
                 ## Random symbol generator code
                 k = 4
@@ -375,7 +291,7 @@ class RTDServer(PubSub):
                     k +=1
 
                 ## make ticks for subscribed symbols
-                for asym in self.add_remove_list:
+                for asym in self.websocket_subscription_list:
                     rec = {"n": asym, "t":t, "d":d, "c": 18 + self.random_generator(), "o": 20 + self.random_generator(), "h": 40 - self.random_generator(), "l": 10 + self.random_generator(), "v": v1, "oi": 0, "bp": self.random_generator(1, 5), "ap": self.random_generator(5, 9), "s": s1, "bs":1, "as":1, "pc":1, "do":20, "dh":40, "dl":10}
                     data.append( rec )
 
